b1a9b9bbad5466cd141bae4c0865faf795c1bfdb
AOA <aoa@d.co>
2025-05-20 20:22:28 +0300

    Initial commit

---
+++ src/main/java/net/alminoris/aestheticstairs/AestheticStairsClient.java
@@ -0,0 +1,12 @@
+package net.alminoris.aestheticstairs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AestheticStairsClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
